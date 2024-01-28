96c1991f5f30c4b9718fb2fe6a5b103856030482
Arjun Singh <arjunsingh15102003@gmail.com>
2024-01-28 22:59:26 +0530

    added api/productbannner route in the backend

--- server.js
+++ server.js
@@ -1,11 +1,19 @@
 const express = require('express');
-const testRoutes = require('./routes/testRoutes.js');
+const cors = require('cors');  // Import the cors middleware
+const productbanner = require('./api/productbanner');
 
 const app = express();
 const port = 5002;
 
-// Use the testRoutes in your express app
-app.use('/test', testRoutes);
+// Use cors middleware to enable cross-origin requests
+app.use(cors());
+
+// Use the productbanner router
+app.use(productbanner);
+
+app.get('/', (req, res) => {
+    res.send('Hello World!');
+});
 
 app.listen(port, () => {
     console.log(`Server running at http://localhost:${port}/`);
